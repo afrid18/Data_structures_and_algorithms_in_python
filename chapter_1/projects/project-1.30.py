a97bbe8f921ac94977ff10ef2cc96304d70ceee8
afrid83 <afridcool83@gmail.com>
2021-04-04 20:44:42 +0530

    Small edit

--- chapter_1/projects/project-1.30.py
+++ chapter_1/projects/project-1.30.py
@@ -1,3 +1,6 @@
+# Project 1.30
+# Write a Python program that can take a positive integer greater than 2 as input and write out the number of times one must repeatedly divide this number by 2 before getting a value less than 2.
+#
 num = int(input("Please Enter an integer equal to or greater than 2:\t"))
 
 steps = 0
