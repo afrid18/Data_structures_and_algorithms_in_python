147001a563cfbb559317de1aae774c26e7e4fae1
afrid83 <afridcool83@gmail.com>
2021-04-10 03:33:20 +0530

    Changes to creditCard.py

--- chapter_2/creditCard.py
+++ chapter_2/creditCard.py
@@ -32,6 +32,10 @@ class CreditCard:
         """Return the card identifying nuber (Typically stored as a string)"""
         return self._account
 
+    def get_limit(self):
+        """Return current credit limit."""
+        return self._limit
+
     def get_balance(self):
         """Return current balance"""
         return self._balance
@@ -51,4 +55,30 @@ class CreditCard:
     def make_payment(self, amount):
         """Process customer payment that reduces balance."""
         self._balance -= amount
+
+
+
+if __name__ == '__main__':
+    wallet = []
+    wallet.append(CreditCard('John Bowman', 'California Savings', '2345 4563 2345 2456', 2500))
+    wallet.append(CreditCard('John Bowman', "california Federal", '2353 7634 9876 4567', 3500))
+    wallet.append(CreditCard('John Bowman', "california Finance", '2353 7564 9876 4567', 5000))
+
+
+for val in range(1, 17):
+    wallet[0].charge(val)
+    wallet[1].charge(2 * val)
+    wallet[2].charge(3 * val)
+
+for c in range(3):
+    print('Customer = ', wallet[c].get_customer())
+    print("Bank = ", wallet[c].get_bank())
+    print('Account = ', wallet[c].get_account())
+    print('Limit = ', wallet[c].get_limit())
+    print('Balance = ', wallet[c].get_balance())
+    while wallet[c].get_balance() > 100:
+        wallet[c].make_payment(100)
+        print('New balance = ', wallet[c].get_balance())
+    print()
+
 
