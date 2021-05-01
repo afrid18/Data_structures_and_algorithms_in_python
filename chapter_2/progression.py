dce5f1d7945dd4bea0e81fca9a883844290dfffb
afrid83 <afridcool83@gmail.com>
2021-05-01 09:02:26 +0530

    Everything in one file 'Progression.py'

--- chapter_2/progression.py
+++ chapter_2/progression.py
@@ -36,3 +36,59 @@ class Progression:
 
     def print_progression(self, n):
         print(' '.join(str(next(self)) for j in range(n)))
+
+
+
+class ArithmeticProgression(Progression):
+    """Iterator producing arithmetic Progression"""
+
+
+    def __init__(self, increment = 1, start = 0):
+        """Create a new arithmetic Progression"""
+        super().__init__(start)
+        self._increment = increment
+
+    def _advance(self):
+
+        self._current += self._increment
+
+
+class GeometricProgression(Progression):
+    """Iterator producing a geometric progression"""
+
+    def __init__(self, base = 2, start = 1):
+
+
+        super().__init__(start)
+        self._base = base
+
+
+    def _advance(self):
+
+        self._current *= self._base
+
+
+
+class FibonacciProgression(Progression):
+
+    def __init__(self, first = 0, second = 1):
+
+        super().__init__(first)
+        self._prev = second - first
+
+
+    def _advance(self):
+
+        self._prev , self._current = self._current , self._prev + self._current
+
+
+
+if __name__ == '__main__':
+    Progression().print_progression(10)
+
+    ArithmeticProgression(5).print_progression(10)
+    ArithmeticProgression(5, 2).print_progression(10)
+    GeometricProgression().print_progression(10)
+    GeometricProgression(3).print_progression(10)
+    FibonacciProgression().print_progression(10)
+    FibonacciProgression(4, 6).print_progression(10)
